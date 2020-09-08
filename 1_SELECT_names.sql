c761c3aec00f9ead92b535891a429c0e7cb3330b
karthykarthick <karthylock@gmail.com>
2020-09-08 20:40:31 +0530

    add sql select queries

--- 1_SELECT_names.sql
+++ 1_SELECT_names.sql
@@ -16,4 +16,18 @@ SELECT name FROM world WHERE name LIKE 'C%' AND name LIKE '%ia'
 --Find the country that has oo in the name
 SELECT name FROM world WHERE name LIKE '%oo%'
 
---
+--Find the countries that have three or more a in the name
+SELECT name FROM world WHERE name LIKE '%a%a%a'
+
+--Find the countries that have "t" as the second character.
+SELECT name FROM world WHERE name LIKE '_t%' ORDER BY name
+
+--Find the countries that have two "o" characters separated by two others.
+SELECT name FROM world WHERE name LIKE '%o__o%'
+
+--Find the countries that have exactly four characters.
+SELECT name FROM world WHERE name LIKE '____'
+
+--Find the country where the name is the capital city.
+SELECT name, capital, continent FROM world WHERE name LIKE capital
+
